316e22e05317a5e427aa942f422abb74b02b71f1
Nekonad <Hidayatnoer11@gamil.com>
2024-03-18 01:23:50 +0700

    1. feat (all section) add semantic html 2. feat (ourGallery) add fitur FancyBox pada img

---
+++ components/loveStory.vue
@@ -0,0 +1,29 @@
+<template>
+  <section id="loveStory">
+    <div align="center" class="title-heading m-0 animate__animated">
+      <h1 class="font-content text-light mb-0">Love Story</h1>
+      <div class="line-separator line-size"></div>
+    </div>
+    <div class="story-list">
+      <div class="story-item text-light">
+        <div class="titleStory font-content text-light">
+          <h4>12 juni 2022</h4>
+        </div>
+        <div class="textStory font-content">hello</div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {};
+</script>
+
+<style lang="css">
+#loveStory {
+  background-color: var(--color-and);
+  padding: 30px 2em;
+  width: 100%;
+}
+
+/* ================================================= */
+</style>
